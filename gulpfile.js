92a062356594bd05188f182a7ae9b9aaff31d454
zvenshy <zvenshy@gmail.com>
2015-03-31 22:12:21 +0800

    add some new gongneng

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
-    gulp.src('./*.scss')
+  gulp.src('./*.scss')
+    .pipe(sourcemaps.init())
       .pipe(sass({errLogToConsole: true}))
-      .pipe(gulp.dest('./css'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
 });
-gulp.task('default', ['sass']);
+
+gulp.task('compress', function () {
+  gulp.src('./css/*.css')
+    .pipe(minify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['sass', 'compress']);
 var watcher = gulp.watch('./*.scss', ['sass']);
 watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
